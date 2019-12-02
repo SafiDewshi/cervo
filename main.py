057d296e011297fa0a1c5af00ef8bf79290ff158
Safi Dewshi <safi.dewshi@gmail.com>
2019-12-02 16:57:58 +0000

    reworked UI some

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import requests
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType("cervo.ui")
+localurl = "http://localhost:3030/cervo/query"
 
 
 class CervoUI(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -13,8 +14,36 @@ class CervoUI(QtWidgets.QMainWindow, Ui_MainWindow):
         self.searchButton.clicked.connect(self.search)
 
     def search(self):
-        searchstring = self.searchBar.text()
-        print(searchstring)
+        searchtext = self.searchBar.text()
+        print("Searching for: " + searchtext)
+        results = self.sparqlsearchspecies(searchtext)
+        print(results)
+
+    def sparqlsearchspecies(self, query):
+        url = localurl
+        sparql = """
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX cervo: <http://www.cervo.io/ontology#>
+        PREFIX dbpedia2: <http://dbpedia.org/property/>
+
+
+        SELECT ?name ?latinname ?zooname
+        WHERE
+        {{
+            ?animal cervo:hasName "{}"^^xsd:string.
+            ?animal cervo:hasLatinName ?latinname.
+            ?animal cervo:hasName ?name.
+            ?animal ^cervo:keeps-animal|cervo:is-kept-at ?zoo.
+            ?zoo cervo:hasName ?zooname
+        }}
+        """
+        sparql.format(query)
+        r = requests.get(url, params={'format': 'json', 'query': query})
+        results = r.json()
+        return results
 
 
 if __name__ == "__main__":
