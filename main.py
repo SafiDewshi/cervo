b749d8e76e454f25338bf2563aee4267464f3ff0
Safi Dewshi <safi.dewshi@gmail.com>
2019-12-02 16:12:54 +0000

    reworked UI some

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-import cervoui
-import requests
+from PyQt5 import uic, QtCore, QtGui, QtWidgets
 import sys
+import requests
+
+Ui_MainWindow, QtBaseClass = uic.loadUiType("cervo.ui")
 
 
-# def search_species(ui, query):
-#     print(query)
-#     ui.results = blah
-# function for each button, which does a query and updates ui
+class CervoUI(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.searchButton.clicked.connect(self.search)
+
+    def search(self):
+        searchstring = self.searchBar.text()
+        print(searchstring)
 
 
-app = cervoui.QtWidgets.QApplication(sys.argv)
-MainWindow = cervoui.QtWidgets.QMainWindow()
-ui = cervoui.Ui_MainWindow()
-ui.setupUi(MainWindow)
-# search_callback = lambda: search_species(ui, ui.searchBar.value)
-# ui.searchButton.
-MainWindow.show()
-sys.exit(app.exec_())
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = CervoUI()
+    window.show()
+    sys.exit(app.exec_())
